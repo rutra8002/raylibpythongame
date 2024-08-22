8823afbe09f0f38375e1b98dec0bfd874768c79a
rutra <arturkummer08@gmail.com>
2024-08-22 16:11:07 +0200

    camera

--- game.py
+++ game.py
@@ -14,17 +14,29 @@ def run_game():
 
     blocks = [Block(50, 500, 100, 600, pyray.BLUE), Block(50, 500, 800, 600, pyray.BLUE), Block(500, 50, 500, 0, pyray.BLUE)]
 
+    camera = pyray.Camera2D()
+    camera.target = pyray.Vector2(player.x + player.width / 2, player.y + player.height / 2)
+    camera.offset = pyray.Vector2(WIDTH / 2, HEIGHT / 2)
+    camera.rotation = 0.0
+    camera.zoom = 1.0
+
     while not pyray.window_should_close():
         delta_time = pyray.get_frame_time()
 
+        camera.target = pyray.Vector2(player.x + player.width / 2, player.y + player.height / 2)
+
         pyray.begin_drawing()
         pyray.clear_background(pyray.WHITE)
+
+        pyray.begin_mode_2d(camera)
 
         player.movement(delta_time, blocks)
         player.draw()
 
         for block in blocks:
             block.draw()
+
+        pyray.end_mode_2d()
 
         pyray.draw_fps(10, 10)
 
