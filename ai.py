fd288081c97778ae0bc6135ea2187ca6c83cb174
rutra <arturkummer08@gmail.com>
2024-08-22 16:46:34 +0200

    added ai collision with player

--- ai.py
+++ ai.py
@@ -38,6 +38,33 @@ class AI:
             elif block.check_horizontal_collision(self) == "right" and self.vx < 0:
                 self.vx = 0
 
+        if self.check_vertical_collision(player) == "top" and self.vy > 0:
+            self.vy = 0
+        elif self.check_vertical_collision(player) == "bottom" and self.vy < 0:
+            self.vy = 0
+        if self.check_horizontal_collision(player) == "left" and self.vx > 0:
+            self.vx = 0
+        elif self.check_horizontal_collision(player) == "right" and self.vx < 0:
+            self.vx = 0
+
         # Update position
         self.x += self.vx * delta_time
         self.y += self.vy * delta_time
+
+    def check_vertical_collision(self, player):
+        touching_top = self.y + self.height > player.y and self.y < player.y
+        touching_bottom = self.y <= player.y + player.height and self.y + self.height > player.y + player.height
+        within_horizontal_bounds = self.x + self.width - 1 > player.x and self.x < player.x + player.width - 1
+        if touching_top and within_horizontal_bounds:
+            return "top"
+        if touching_bottom and within_horizontal_bounds:
+            return "bottom"
+
+    def check_horizontal_collision(self, player):
+        touching_left = self.x + self.width > player.x and self.x < player.x
+        touching_right = self.x < player.x + player.width and self.x + self.width > player.x + player.width
+        within_vertical_bounds = self.y + self.height - 1 > player.y and self.y < player.y + player.height - 1
+        if touching_left and within_vertical_bounds:
+            return "left"
+        if touching_right and within_vertical_bounds:
+            return "right"
