2a198c2c6d7bc4329b68ae6be5562fece8a60cc7
rutra <arturkummer08@gmail.com>
2024-09-18 20:36:09 +0200

    movement no tolerance thing

--- player.py
+++ player.py
@@ -14,7 +14,6 @@ class Player(GameObject):
 
     def movement(self, delta_time, blocks):
         self.grounded = False
-        tolerance = 0.1
 
         for block in blocks:
             vertical_collision = block.check_vertical_collision(self)
@@ -22,21 +21,20 @@ class Player(GameObject):
 
             if vertical_collision == "top":
                 self.grounded = True
-                self.y = block.y - self.height + tolerance  #tolerance
-                self.vy = 0
                 if pyray.is_key_down(pyray.KeyboardKey.KEY_SPACE):
                     self.vy = -self.jump
                     self.grounded = False
             elif vertical_collision == "bottom":
-                self.y = block.y + block.height  #notolerance
-                self.vy = -self.vy / 2
+                if self.vy < 0:
+                    self.vy = -self.vy / 2
 
             if horizontal_collision == "left":
-                self.x = block.x - self.width  #notolerance
-                self.vx = 0
+                if self.vx > 0:
+                    self.vx = 0
+
             elif horizontal_collision == "right":
-                self.x = block.x + block.width #notolerance
-                self.vx = 0
+                if self.vx < 0:
+                    self.vx = 0
 
         if not self.grounded:
             self.vy += self.gravity * delta_time * self.mass
