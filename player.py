9108739468b5983e96a23a6118ca1b1a046102c8
rutra <arturkummer08@gmail.com>
2024-09-18 20:15:52 +0200

    movement

--- player.py
+++ player.py
@@ -6,35 +6,44 @@ class Player(GameObject):
         super().__init__(height, width, x, y, color)
         self.vy = 0
         self.vx = 0
-        self.gravity = -9.81
+        self.gravity = 9.81
         self.speed = 250
-        self.jump = -400
+        self.jump = 400
         self.mass = mass
         self.grounded = False
 
     def movement(self, delta_time, blocks):
+        self.grounded = False
+        tolerance = 0.1
+
         for block in blocks:
-            if block.check_vertical_collision(self) == "top":
+            vertical_collision = block.check_vertical_collision(self)
+            horizontal_collision = block.check_horizontal_collision(self)
+
+            if vertical_collision == "top":
+                self.grounded = True
+                self.y = block.y - self.height + tolerance  #tolerance
+                self.vy = 0
                 if pyray.is_key_down(pyray.KeyboardKey.KEY_SPACE):
-                    self.vy = self.jump
+                    self.vy = -self.jump
                     self.grounded = False
-                else:
-                    self.grounded = True
-            elif block.check_vertical_collision(self) == "bottom":
-                if self.vy < 0:
-                    self.vy = -self.vy / 2
-            if block.check_horizontal_collision(self) == "left":
-                if self.vx > 0:
-                    self.vx = 0
-            elif block.check_horizontal_collision(self) == "right":
-                if self.vx < 0:
-                    self.vx = 0
+            elif vertical_collision == "bottom":
+                self.y = block.y + block.height  #notolerance
+                self.vy = -self.vy / 2
+
+            if horizontal_collision == "left":
+                self.x = block.x - self.width + tolerance  #tolerance
+                self.vx = 0
+            elif horizontal_collision == "right":
+                self.x = block.x + block.width - tolerance  #tolerance
+                self.vx = 0
 
         if not self.grounded:
-            self.vy -= self.gravity * delta_time * self.mass
+            self.vy += self.gravity * delta_time * self.mass
         else:
             self.vy = 0
-        self.vx = self.vx * 0.9
+
+        self.vx *= 0.9  # Apply friction
         self.y += self.vy * delta_time
         self.x += self.vx * delta_time
 
