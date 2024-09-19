27ba658bbd3760783a6c9fa580d1af390c87db91
artur <arturkummer08@gmail.com>
2024-09-19 10:31:51 +0200

    nothing tbh

--- game.py
+++ game.py
@@ -13,14 +13,14 @@ class Game:
         self.player = Player(50, 50, 100, 100, pyray.RED, 70)
         self.blocks = [Block(50, 500, 100, 600, pyray.BLUE),
                        Block(50, 500, 800, 600, pyray.BLUE),
-                       Block(50, 500, 1500, 600, pyray.BLUE),
+                       Block(50, 50000, 1500, 600, pyray.BLUE),
                        Block(50, 100, 650, 500, pyray.BLUE),
                        Block(500, 50, 500, 0, pyray.BLUE),
                        Block(500, 200, 850, 0, pyray.BLUE),
                        Block(500, 50, 500, 900, pyray.BLUE),
                        Block(50, 550, 500, 1400, pyray.BLUE),
                        Block(500, 50, 1000, 900, pyray.BLUE),]
-        self.camera = Camera(width, height, self.player.x + self.player.width / 2, self.player.y + self.player.height / 2, 1)
+        self.camera = Camera(width, height, self.player.x + self.player.width / 2, self.player.y + self.player.height / 2, 3)
         self.particle_system = ParticleSystem()
         self.particle_update_timer = 0
 
