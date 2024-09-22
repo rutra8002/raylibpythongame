2bdbba549a5c73a4d6ca5352c8a0303d778089db
rutra <arturkummer08@gmail.com>
2024-09-22 12:54:47 +0200

    camera unzoom

--- camera.py
+++ camera.py
@@ -13,6 +13,13 @@ class Camera:
         self.camera.target.x += (target_x - self.camera.target.x) * self.smooth_factor * delta_time
         self.camera.target.y += (target_y - self.camera.target.y) * self.smooth_factor * delta_time
 
+    def adjust_zoom(self, player_speed, delta_time):
+        if abs(player_speed) > 400:
+            target_zoom = 400/abs(player_speed)
+        else:
+            target_zoom = 1
+        self.camera.zoom += (target_zoom - self.camera.zoom) * self.smooth_factor * delta_time
+
     def begin_mode(self):
         pyray.begin_mode_2d(self.camera)
 
