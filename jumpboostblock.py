f416f35453c7da395e9251b36afb74547a07caa0
rutra4 <asda@asda>
2024-09-23 12:28:51 +0200

    op textures

--- jumpboostblock.py
+++ jumpboostblock.py
@@ -1,9 +1,14 @@
+import pyray
+
 from block import Block
 
 class JumpBoostBlock(Block):
     def __init__(self, height, width, x, y, color, jump_boost):
         super().__init__(height, width, x, y, color)
         self.jump_boost = jump_boost
+        self.texture = pyray.load_texture("images/jump.png")
+        if self.texture.id == 0:
+            raise ValueError("Failed to load texture")
 
     def check_vertical_collision(self, other):
         collision_side = super().check_vertical_collision(other)
@@ -11,3 +16,8 @@ class JumpBoostBlock(Block):
             other.grounded = False
             other.vy = -self.jump_boost
         return collision_side
+
+    def draw(self):
+        for i in range(0, self.width, self.texture.width):
+            for j in range(0, self.height, self.texture.height):
+                pyray.draw_texture(self.texture, int(self.x + i), int(self.y + j), pyray.WHITE)
--- speedboostblock.py
+++ speedboostblock.py
@@ -1,9 +1,15 @@
 from block import Block
 
+import pyray
+
 class SpeedBoostBlock(Block):
     def __init__(self, height, width, x, y, color, speed_boost):
         super().__init__(height, width, x, y, color)
         self.speed_boost = speed_boost
+        self.texture = pyray.load_texture("images/speeed.png")
+        if self.texture.id == 0:
+            raise ValueError("Failed to load texture")
+
 
     def check_vertical_collision(self, other):
         collision_side = super().check_vertical_collision(other)
@@ -15,3 +21,8 @@ class SpeedBoostBlock(Block):
             else:
                 pass
         return collision_side
+
+    def draw(self):
+        for i in range(0, self.width, self.texture.width):
+            for j in range(0, self.height, self.texture.height):
+                pyray.draw_texture(self.texture, int(self.x + i), int(self.y + j), pyray.WHITE)
