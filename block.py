7333b48838184db35649174eed1f12012ca2d3dc
rutra4 <asda@asda>
2024-09-23 12:05:32 +0200

    ez

--- block.py
+++ block.py
@@ -1,5 +1,14 @@
+import pyray
 from gameobject import GameObject
 
 class Block(GameObject):
     def __init__(self, height, width, x, y, color):
         super().__init__(height, width, x, y, color)
+        self.texture = pyray.load_texture("images/block.png")
+        if self.texture.id == 0:
+            raise ValueError("Failed to load texture")
+
+    def draw(self):
+        for i in range(0, self.width, self.texture.width):
+            for j in range(0, self.height, self.texture.height):
+                pyray.draw_texture(self.texture, int(self.x + i), int(self.y + j), pyray.WHITE)
