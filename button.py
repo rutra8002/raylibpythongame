0b671cf3bce9ebb54324ed4b6dddcf785bb35c34
rutra <arturkummer08@gmail.com>
2024-10-06 19:29:33 +0200

    optimised button.py xd

--- button.py
+++ button.py
@@ -1,3 +1,4 @@
+import raylib
 import pyray
 
 class Button:
@@ -13,9 +14,9 @@ class Button:
         self.is_clicked = False
 
     def update(self):
-        mouse_point = pyray.get_mouse_position()
-        self.is_hovered = pyray.check_collision_point_rec(mouse_point, self.rect)
-        self.is_clicked = self.is_hovered and pyray.is_mouse_button_pressed(pyray.MouseButton.MOUSE_BUTTON_LEFT)
+        mouse_point = raylib.GetMousePosition()
+        self.is_hovered = raylib.CheckCollisionPointRec(mouse_point, self.rect)
+        self.is_clicked = self.is_hovered and raylib.IsMouseButtonPressed(raylib.MOUSE_BUTTON_LEFT)
 
     def draw(self):
         if self.is_clicked:
@@ -25,8 +26,8 @@ class Button:
         else:
             color = self.button_color
 
-        pyray.draw_rectangle_rec(self.rect, color)
-        text_width = pyray.measure_text(self.text, self.text_size)
+        raylib.DrawRectangleRec(self.rect, color)
+        text_width = raylib.MeasureText(self.text.encode('utf-8'), self.text_size)
         text_x = self.rect.x + (self.rect.width - text_width) / 2
         text_y = self.rect.y + (self.rect.height - self.text_size) / 2
-        pyray.draw_text(self.text, int(text_x), int(text_y), self.text_size, self.text_color)
+        raylib.DrawText(self.text.encode('utf-8'), int(text_x), int(text_y), self.text_size, self.text_color)
