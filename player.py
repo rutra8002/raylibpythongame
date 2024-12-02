21f83f8da42b649dd084b4223b07d4e9fe67a926
ruter <arturkummer08@gmail.com>
2024-12-02 21:11:49 +0100

    make player move in air faster

--- player.py
+++ player.py
@@ -157,10 +157,10 @@ class Player(GameObject):
                 self.vy += self.gravity * delta_time * self.mass
             if pyray.is_key_down(pyray.KeyboardKey.KEY_D):
                 if not any(block.check_horizontal_collision(self) == "left" for block in blocks):
-                    self.vx += 0.00025 * self.speed
+                    self.vx += 0.00045 * self.speed
             if pyray.is_key_down(pyray.KeyboardKey.KEY_A):
                 if not any(block.check_horizontal_collision(self) == "right" for block in blocks):
-                    self.vx += 0.00025 * -self.speed
+                    self.vx += 0.00045 * -self.speed
             self.vx -= 0.0001 * self.vx
         else:
             if pyray.is_key_down(pyray.KeyboardKey.KEY_LEFT_CONTROL):
