e771d4505f36c450bfae2a3fa55ed43069cae4b1
ruter <arturkummer08@gmail.com>
2024-12-30 10:09:27 +0100

    maps and lavablock

--- blocks/lavablock.py
+++ blocks/lavablock.py
@@ -11,17 +11,21 @@ class LavaBlock(Block):
     def check_vertical_collision(self, other):
         collision_side = super().check_vertical_collision(other)
         if collision_side:
+            other.can_jump = False
             other.speed = 6.9
             if other.time_since_last_damage > 0.1:
                 other.take_damage(10)
                 other.time_since_last_damage = 0
-        return collision_side
+                other.vx = 0
+                other.vy = 0
 
     def check_horizontal_collision(self, other):
         collision_side = super().check_horizontal_collision(other)
         if collision_side:
+            other.can_jump = False
             other.speed = 6.9
             if other.time_since_last_damage > 0.1:
                 other.take_damage(10)
                 other.time_since_last_damage = 0
-        return collision_side
+                other.vx = 0
+                other.vy = 0
