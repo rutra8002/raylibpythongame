c02b2c5afbc798d90f3505a53e09cdd82b1d0d1e
ruter <arturkummer08@gmail.com>
2024-12-30 21:35:35 +0100

    collision fix

--- gameobject.py
+++ gameobject.py
@@ -12,8 +12,8 @@ class GameObject:
         pyray.draw_rectangle(int(self.x), int(self.y), self.width, self.height, self.color)
 
     def check_vertical_collision(self, other):
-        touching_top = other.y + other.height+1 > self.y and other.y-1 < self.y
-        touching_bottom = other.y +1 < self.y + self.height and other.y + other.height -1> self.y + self.height
+        touching_top = other.y + other.height> self.y and other.y < self.y
+        touching_bottom = other.y <= self.y + self.height and other.y + other.height > self.y + self.height
         within_horizontal_bounds = other.x + other.width -1 > self.x and other.x < self.x + self.width -1
         if touching_top and within_horizontal_bounds:
             return "top"
@@ -21,9 +21,9 @@ class GameObject:
             return "bottom"
 
     def check_horizontal_collision(self, other):
-        touching_left = other.x + other.width +1> self.x and other.x-1 < self.x
-        touching_right = other.x +1 < self.x + self.width and other.x + other.width -1 > self.x + self.width
-        within_vertical_bounds = other.y + other.height - 1 > self.y and other.y < self.y + self.height - 1
+        touching_left = other.x + other.width> self.x and other.x < self.x
+        touching_right = other.x < self.x + self.width and other.x + other.width > self.x + self.width
+        within_vertical_bounds = other.y + other.height -1 > self.y and other.y < self.y + self.height -1
         if touching_left and within_vertical_bounds:
             return "left"
         if touching_right and within_vertical_bounds:
