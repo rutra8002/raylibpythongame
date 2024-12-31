6693a6e9c9ce6f03aa72882b1999ffdff3503e05
ruter <arturkummer08@gmail.com>
2024-12-31 15:06:26 +0100

    Add resolution and fullscreen toggle to settings menu
    Introduced functionality to adjust screen resolution and toggle fullscreen mode in the settings menu. Users can now cycle through predefined resolutions or enable/disable fullscreen with corresponding buttons. This enhances user control over display settings directly within the UI.

--- UI/main_menu.py
+++ UI/main_menu.py
@@ -15,6 +15,9 @@ class MainMenu:
         self.show_settings = False
         self.maps = []
         self.selected_map = None
+        self.fullscreen = False
+        self.resolutions = [(1366, 768), (1920, 1080), (2560, 1440)]
+        self.current_resolution_index = 0
         self.start_button = Button(width / 2 - 100, height / 2 - 50, 200, 50, "Start Game", 20, pyray.WHITE,
                                    pyray.DARKGRAY, pyray.GRAY, pyray.LIGHTGRAY)
         self.settings_button = Button(width / 2 - 100, height / 2 + 10, 200, 50, "Settings", 20, pyray.WHITE,
@@ -115,14 +118,38 @@ class MainMenu:
             shaders_button_text = "Enabled" if shaders.shaders_enabled else "Disabled"
             shaders_button = Button(200, 200, 200, 50, shaders_button_text, 20,
                                     pyray.WHITE, pyray.DARKGRAY, pyray.GRAY, pyray.LIGHTGRAY)
-            back_button = Button(self.width / 2 - 100, self.height / 2 + 70, 200, 50, "Back", 20, pyray.WHITE,
-                                 pyray.DARKGRAY, pyray.GRAY, pyray.LIGHTGRAY)
-
             shaders_button.update()
             shaders_button.draw()
             if shaders_button.is_clicked:
                 shaders.shaders_enabled = not shaders.shaders_enabled
 
+            resolution_text = "Resolution"
+            pyray.draw_text(resolution_text, 50, 275, 20, pyray.WHITE)
+
+            resolution_button_text = f"{self.resolutions[self.current_resolution_index][0]}x{self.resolutions[self.current_resolution_index][1]}"
+            resolution_button = Button(200, 260, 200, 50, resolution_button_text, 20,
+                                       pyray.WHITE, pyray.DARKGRAY, pyray.GRAY, pyray.LIGHTGRAY)
+            resolution_button.update()
+            resolution_button.draw()
+            if resolution_button.is_clicked:
+                self.current_resolution_index = (self.current_resolution_index + 1) % len(self.resolutions)
+                self.width, self.height = self.resolutions[self.current_resolution_index]
+                pyray.set_window_size(self.width, self.height)
+
+            fullscreen_text = "Fullscreen"
+            pyray.draw_text(fullscreen_text, 50, 335, 20, pyray.WHITE)
+
+            fullscreen_button_text = "Enabled" if self.fullscreen else "Disabled"
+            fullscreen_button = Button(200, 320, 200, 50, fullscreen_button_text, 20,
+                                       pyray.WHITE, pyray.DARKGRAY, pyray.GRAY, pyray.LIGHTGRAY)
+            fullscreen_button.update()
+            fullscreen_button.draw()
+            if fullscreen_button.is_clicked:
+                self.fullscreen = not self.fullscreen
+                pyray.toggle_fullscreen()
+
+            back_button = Button(self.width / 2 - 100, self.height / 2 + 70, 200, 50, "Back", 20, pyray.WHITE,
+                                 pyray.DARKGRAY, pyray.GRAY, pyray.LIGHTGRAY)
             back_button.update()
             back_button.draw()
             if back_button.is_clicked:
